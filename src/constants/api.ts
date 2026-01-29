15cb6667f96da25fc6ab75f72d0f747993c86c61
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-01-29 16:29:58 +0330

    fix: font. add: to body.

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,16 +1,22 @@
-import img1 from "./assets/carImages/b-1.png";
+// import img1 from "./assets/carImages/b-1.png";
 
-const delay = (time) => {
-  setTimeout(() => {}, time);
-};
+// const delay = (time) => {
+//   setTimeout(() => {}, time);
+// };
 
-export const getItems = async () => {
-  return [
-    {
-      id: 1,
-      title: "گوشی اپل",
-      price: 120000000,
-      image: img1,
-    },
-  ];
-};
+// export const getItems = async () => {
+//   return [
+//     {
+//       id: 1,
+//       title: "گوشی اپل",
+//       price: 120000000,
+//       image: img1,
+//     },
+//   ];
+// };
+export const BASE_URL = "http://localhost:3400/";
+
+// Headers:{
+//   "Content-Type": "application/json",
+// }
+// import.meta.env.VITE_BASE_URL;
