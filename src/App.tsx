f1a93fed06ca4ef0ae5d0a05da9e0d6be6e4f503
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-02-12 21:35:19 +0330

    add: react query and config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./router/Router";
 import { Toaster } from "sonner";
+import defaultOptions from "./config/reactQuery";
 
 function App() {
+  const queryClient = new QueryClient({ defaultOptions });
   return (
     <>
-      {/* <AuthPage />
-      <Toaster /> */}
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <Toaster />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+        <Toaster />
+      </QueryClientProvider>
     </>
   );
 }
