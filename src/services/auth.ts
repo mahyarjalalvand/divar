7408b79a1fde2ba4afbbd96a1cb48cd4cf135470
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-03-03 22:03:07 +0330

    add: function for handle 401 error and get user profile

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,9 @@
+import { getCookie, setCookie } from "@/utils/cookie";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 const sendOtp = async (mobile: string) => {
   try {
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}auth/send-otp`, {
+    const response = await fetch(`${BASE_URL}auth/send-otp`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -22,7 +25,7 @@ const sendOtp = async (mobile: string) => {
 };
 const checkOtp = async (mobile: string, code: string) => {
   try {
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}auth/check-otp`, {
+    const response = await fetch(`${BASE_URL}auth/check-otp`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -42,4 +45,45 @@ const checkOtp = async (mobile: string, code: string) => {
     };
   }
 };
-export { sendOtp, checkOtp };
+const fetchWithAuth = async (url: string, options: RequestInit = {}) => {
+  let accessToken = getCookie("accessToken");
+  const res = await fetch(BASE_URL + url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (res.status === 401) {
+    const refreshToken = getCookie("refreshToken");
+    console.log(refreshToken);
+    if (!refreshToken) throw new Error("Refresh token missing");
+
+    const refreshRes = await fetch(`${BASE_URL}auth/check-refresh-token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ refreshToken }),
+    });
+
+    if (!refreshRes.ok) throw new Error("Refresh token expired");
+
+    const tokens = await refreshRes.json();
+    setCookie(tokens);
+
+    accessToken = tokens.accessToken;
+    return fetch(BASE_URL + url, {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+        ...(options.headers || {}),
+      },
+    });
+  }
+
+  return res;
+};
+export { sendOtp, checkOtp, fetchWithAuth };
