e79cb57df7467156aa5e675171b2c9adcda3ff3a
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-03-03 22:16:05 +0330

    use react query for get data and handle loading

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,17 @@
+import { Spinner } from "@/components/ui/spinner";
 import PageNotFound from "@/pages/404";
 import AdminPage from "@/pages/AdminPage";
 import AuthPage from "@/pages/AuthPage";
 import DashboardPage from "@/pages/DashboardPage";
 import HomePage from "@/pages/HomePage";
+import { getProfile } from "@/services/user";
+import { useQuery } from "@tanstack/react-query";
 import { Route, Routes } from "react-router-dom";
 
 function Router() {
+  const { data, isLoading, error } = useQuery({ queryKey: ["profile"], queryFn: getProfile });
+  console.log(data, error, isLoading);
+  if (isLoading) return <Spinner className="size-8" />;
   return (
     <Routes>
       <Route index element={<HomePage />} />
