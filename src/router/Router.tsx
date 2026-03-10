d8b5c36c202e1ed61ceb3de3e401c85a549e8059
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-03-10 16:32:40 +0330

    add: apply user access to pages

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -6,7 +6,7 @@ import DashboardPage from "@/pages/DashboardPage";
 import HomePage from "@/pages/HomePage";
 import { getProfile } from "@/services/user";
 import { useQuery } from "@tanstack/react-query";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function Router() {
   const { data, isLoading, error } = useQuery({ queryKey: ["profile"], queryFn: getProfile });
@@ -15,9 +15,9 @@ function Router() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="/admin" element={<AdminPage />} />
+      <Route path="/dashboard" element={data ? <DashboardPage /> : <Navigate to="/auth" />} />
+      <Route path="/auth" element={data ? <Navigate to="/dashboard" /> : <AuthPage />} />
+      <Route path="/admin" element={data && data.data.role === "ADMIN" ? <AdminPage /> : <Navigate to="/" />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
