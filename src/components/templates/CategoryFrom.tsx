6b73979346ea1323fa63bc7a6c95930312e27442
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-03-19 19:34:31 +0330

    add: component for get data from admin for create category

---
+++ src/components/templates/CategoryFrom.tsx
@@ -0,0 +1,49 @@
+import addCategory from "@/services/admin";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+export interface FormStateType {
+  name: string;
+  slug: string;
+  icon: string;
+}
+function CategoryFrom() {
+  // todo add type for fc event
+  const [form, setForm] = useState<FormStateType>({ name: "", slug: "", icon: "" });
+
+  const { mutate, isLoading, error, data } = useMutation(addCategory);
+
+  console.log({ error });
+
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // if (!form.name || !form.slug || !form.icon) return;
+    // console.log(form);
+    mutate(form);
+  };
+
+  return (
+    <form
+      onChange={changeHandler}
+      onSubmit={submitHandler}
+      className="[&_label]:block [&_label]:text-sm [&_label]:mb-2.5 [&_input]:block [&_input]:w-75 [&_input]:p-2.5 [&_input]:border [&_input]:border-gray-300 [&_input]:rounded-sm [&_input]:mb-7.5 [&_p]:bg-red-800 [&_p]:text-white [&_p]:mb-5 [&_p]:p-1.5 [&_p]:text-center [&_p]:rounded-sm">
+      <h3 className="mb-7.5 border-b-2 border-red-800 w-fit pb-1.5">دسته بندی جدید</h3>
+      {!!error && <p>مشکلی پیش آمده است</p>}
+      {data?.status === 201 && <p>دسته بندی با موفقیت ایجاد شد</p>}
+      <label htmlFor="name">نام دسته بندی</label>
+      <input type="text" name="name" id="name" />
+      <label htmlFor="slug">اسلاگ</label>
+      <input type="text" name="slug" id="slug" />
+      <label htmlFor="icon">ایکن</label>
+      <input type="text" name="icon" id="icon" />
+      <button type="submit" className="bg-red-800 text-white py-2.5 px-6.25 text-sm cursor-pointer rounded-sm">
+        ایجاد
+      </button>
+    </form>
+  );
+}
+
+export default CategoryFrom;
