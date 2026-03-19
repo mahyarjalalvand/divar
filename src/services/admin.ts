3cd588bf8ad58556770b9a0a04d49b7914c4a0b1
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-03-19 19:32:40 +0330

    add: function for get all category

--- src/services/admin.ts
+++ src/services/admin.ts
@@ -1,5 +1,4 @@
 import type { FormStateType } from "@/components/templates/CategoryFrom";
-
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const addCategory = async (data: FormStateType) => {
   try {
@@ -14,6 +13,22 @@ const addCategory = async (data: FormStateType) => {
     return res;
   } catch (error) {
     console.log(error);
+    return error;
+  }
+};
+const getCategory = async () => {
+  try {
+    const req = await fetch(`${BASE_URL}category`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const res = await req.json();
+    return res;
+  } catch (error) {
+    console.log(error);
+    return error;
   }
 };
 export default addCategory;
