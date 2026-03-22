761cfb8965ff9e9ad8b16ee8a8461223975be4cc
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-03-22 23:19:10 +0330

    add: main component query and sidebar component query for handle loading

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,24 @@
 import Main from "@/components/templates/Main";
 import Sidebar from "@/components/templates/Sidebar";
+import { Spinner } from "@/components/ui/spinner";
+import { getCategory } from "@/services/admin";
+import { getAllPosts } from "@/services/user";
+import { useQuery } from "@tanstack/react-query";
 
 function HomePage() {
+  const { data: posts, isLoading: postLoading } = useQuery({ queryKey: ["post-list"], queryFn: getAllPosts });
+  const { data: categories, isLoading: categoriesLoading } = useQuery({ queryKey: ["get-categories"], queryFn: getCategory });
   return (
-    <div className="flex">
-      <Main />
-      <Sidebar />
-    </div>
+    <>
+      {postLoading || categoriesLoading ? (
+        <Spinner className="size-10" />
+      ) : (
+        <div className="flex">
+          <Main posts={posts} />
+          <Sidebar categories={categories} />
+        </div>
+      )}
+    </>
   );
 }
 
