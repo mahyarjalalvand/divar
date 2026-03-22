0198ee2994657b6baabd6a97c37ddd0c3d109229
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-03-22 23:16:14 +0330

    add: add: function for get all posts

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,10 +1,27 @@
 import { fetchWithAuth } from "./auth";
 
 // import { toast, Toaster } from "sonner";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 const getProfile = async () => {
   const res = await fetchWithAuth("user/whoami");
   const data = await res.json();
   return data;
 };
 
-export { getProfile };
+const getAllPosts = async () => {
+  try {
+    const req = await fetch(BASE_URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const res = await req.json();
+    return res;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+export { getProfile, getAllPosts };
