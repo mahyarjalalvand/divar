4a583eed21890366283316045dc935edbc5d0db4
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-03-22 22:14:34 +0330

    add: sidebar and main component in home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Main from "@/components/templates/Main";
+import Sidebar from "@/components/templates/Sidebar";
 
 function HomePage() {
-  return <div>home</div>;
+  return (
+    <div className="flex">
+      <Main />
+      <Sidebar />
+    </div>
+  );
 }
 
 export default HomePage;
