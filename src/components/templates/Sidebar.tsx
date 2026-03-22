fc1fe1addacb5ed55a712420524535f3ec34b2a8
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-03-22 23:15:17 +0330

    add: html structure and add loop for data and display categories

--- src/components/templates/Sidebar.tsx
+++ src/components/templates/Sidebar.tsx
@@ -1,10 +1,19 @@
-import { getCategory } from "@/services/admin";
-import { useQuery } from "@tanstack/react-query";
-
-function Sidebar() {
-  const { data } = useQuery({ queryKey: ["get-categories"], queryFn: getCategory });
+// todo . add type for getting data from api
+function Sidebar({ categories }) {
   //   console.log(data)
-  return <div>sidebar</div>;
+  return (
+    <div className="mt-7.5 w-50">
+      <h4>دسته ها</h4>
+      <ul>
+        {categories.data?.map((cat) => (
+          <li key={cat._id} className="flex my-5">
+            <img src={`${cat.icon}.svg`} alt="" />
+            <p className="font-extralight ms-2.5 text-gray-400">{cat.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
