0f0f05b7275289e0d602c2ecdc1baa6b64f6cd58
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-04-29 16:15:19 +0330

    add: jsx and loading

--- src/components/templates/CategoryList.tsx
+++ src/components/templates/CategoryList.tsx
@@ -1,5 +1,29 @@
+import { getCategory } from "@/services/admin";
+import { useQuery } from "@tanstack/react-query";
+import { Spinner } from "../ui/spinner";
+interface CategoryDataType {
+  name: string;
+  _id: number;
+  icon: string;
+  slug: string;
+}
 function CategoryList() {
-  return <div></div>;
+  const { data, isLoading } = useQuery({ queryKey: ["get-categories"], queryFn: getCategory });
+  return (
+    <div className="mt-12.5 mb-17.5 [&_div]:flex [&_div]:my-5 [&_div]:p-4 [&_div]:border-2 [&_div]:bg-gray-200 [&_div]:rounded-sm">
+      {isLoading ? (
+        <Spinner className="size-10" />
+      ) : (
+        data.data?.map((item: CategoryDataType) => (
+          <div key={item._id}>
+            <img src={`${item.icon}.svg`} alt="" />
+            <h5 className="ms-2.5 text-sm w-30">{item.name}</h5>
+            <p className="w-full text-end text-red-800">slug:{item.slug}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default CategoryList;
