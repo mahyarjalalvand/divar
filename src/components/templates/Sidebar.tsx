ac5ef155308df646a102ae6ef9349636b3c4decf
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-07-31 18:54:41 +0330

    fix: component type

--- src/components/templates/Sidebar.tsx
+++ src/components/templates/Sidebar.tsx
@@ -1,12 +1,19 @@
+import type { CategoryType } from "@/types/category";
+
 // todo . add type for getting data from api
-function Sidebar({ categories }) {
-  //   console.log(data)
+
+interface SidebarProps {
+  categories: CategoryType[];
+}
+
+function Sidebar({ categories }: SidebarProps) {
   return (
     <div className="mt-7.5 w-50">
       <h4>دسته ها</h4>
       <ul>
-        {categories.data?.map((cat) => (
+        {categories?.map((cat) => (
           <li key={cat._id} className="flex my-5">
+            <a href=""></a>
             <img src={`${cat.icon}.svg`} alt="" />
             <p className="font-extralight ms-2.5 text-gray-400">{cat.name}</p>
           </li>
