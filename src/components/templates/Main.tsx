75c06c1e91ee95ebc74728552e89fcf7449b57b3
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-07-31 13:36:16 +0330

    use: posts type in component. use: sp function for show amount

--- src/components/templates/Main.tsx
+++ src/components/templates/Main.tsx
@@ -1,14 +1,17 @@
-function Main({ posts }) {
+import type { PostsResponse } from "@/types/post";
+import { sp } from "@/utils/numbers";
+
+function Main({ posts }: PostsResponse) {
   const BASE_URL = import.meta.env.VITE_BASE_URL;
-  // todo create single page for agahi
+
   return (
     <div className=" flex flex-wrap justify-between mt-5 w-[calc(100%-200px)]">
-      {posts.data?.posts.map((post) => (
+      {posts?.map((post) => (
         <div key={post._id} className="w-82.5 flex justify-between border border-gray-200 m-2.5 rounded-sm ">
           <div className="flex justify-between flex-col">
-            <p>{post.options?.title}</p>
+            <p>{post.options.title}</p>
             <div className="text-gray-400 text-sm">
-              <p>{/* see part 381 for add and use sp functions */}</p>
+              <p>{sp(post.amount)} تومان</p>
               <span>{post.options?.city}</span>
             </div>
           </div>
