8d51488b430126deaad1e16279e58b31532306d1
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-07-31 18:55:40 +0330

    add: container for spinnet. fix: components prop

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,16 +6,19 @@ import { getAllPosts } from "@/services/user";
 import { useQuery } from "@tanstack/react-query";
 
 function HomePage() {
-  const { data: posts, isLoading: postLoading } = useQuery({ queryKey: ["post-list"], queryFn: getAllPosts });
+  const { data: postsRes, isLoading: postLoading } = useQuery({ queryKey: ["post-list"], queryFn: getAllPosts });
   const { data: categories, isLoading: categoriesLoading } = useQuery({ queryKey: ["get-categories"], queryFn: getCategory });
+  // console.log(categories);
   return (
     <>
       {postLoading || categoriesLoading ? (
-        <Spinner className="size-10" />
+        <div className="center w-full h-[50svh]">
+          <Spinner className="size-10" />
+        </div>
       ) : (
         <div className="flex container">
-          <Main posts={posts} />
-          <Sidebar categories={categories} />
+          <Sidebar categories={categories ?? []} />
+          <Main posts={postsRes?.posts ?? []} />
         </div>
       )}
     </>
