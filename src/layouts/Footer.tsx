c54a5e77c9c759f7802f485689cf31215ddab9d1
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-07-31 22:49:27 +0330

    delete: mahyar in footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="text-center dir-ltr mt-25 pt-2.5 border-2 border-gray-100">
-      <p>Developed by Mahyar with love</p>
+      <p>Developed by Mahyar</p>
     </footer>
   );
 }
