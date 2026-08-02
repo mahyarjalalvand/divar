54b7e7f38ed5a6f859ede324f423a87ac7c16f5c
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-02 15:34:41 +0330

    add: value and onchange for inputs. fix: changeHandler function

--- src/components/templates/CategoryFrom.tsx
+++ src/components/templates/CategoryFrom.tsx
@@ -27,8 +27,11 @@ function CategoryFrom() {
     },
   });
 
-  const changeHandler = (event: React.ChangeEvent<HTMLFormElement>) => {
-    setForm({ ...form, [event.target?.name]: event.target.value });
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
   };
 
   const submitHandler = (e: React.SubmitEvent<HTMLFormElement>) => {
@@ -37,22 +40,24 @@ function CategoryFrom() {
       toast.error("لطفا مقادیر را کامل پر کنید");
       return;
     }
-
-    mutate(form);
+    mutate(form, {
+      onSuccess: () => {
+        setForm({ name: "", slug: "", icon: "" });
+      },
+    });
   };
 
   return (
     <form
-      onChange={changeHandler}
       onSubmit={submitHandler}
       className="[&_label]:block [&_label]:text-sm [&_label]:mb-2.5 [&_input]:block [&_input]:w-75 [&_input]:p-2.5 [&_input]:border [&_input]:border-gray-300 [&_input]:rounded-sm [&_input]:mb-7.5 [&_p]:bg-red-800 [&_p]:text-white [&_p]:mb-5 [&_p]:p-1.5 [&_p]:text-center [&_p]:rounded-sm">
       <h3 className="mb-7.5 border-b-2 border-red-800 w-fit pb-1.5">دسته بندی جدید</h3>
       <label htmlFor="name">نام دسته بندی</label>
-      <input type="text" name="name" id="name" />
+      <input type="text" name="name" id="name" value={form.name} onChange={changeHandler} />
       <label htmlFor="slug">اسلاگ</label>
-      <input type="text" name="slug" id="slug" />
+      <input type="text" name="slug" id="slug" value={form.slug} onChange={changeHandler} />
       <label htmlFor="icon">ایکن</label>
-      <input type="text" name="icon" id="icon" />
+      <input type="text" name="icon" id="icon" value={form.icon} onChange={changeHandler} />
       <button type="submit" disabled={isPending} className="bg-red-800 text-white py-2.5 px-6.25 text-sm cursor-pointer rounded-sm">
         {isPending ? "در حال ثبت..." : "ایجاد"}
       </button>
