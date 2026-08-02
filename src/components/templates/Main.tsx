a00a15b553a207bb30c54c049fbe96f0b3262f83
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-02 18:04:34 +0330

    fix: post title and city

--- src/components/templates/Main.tsx
+++ src/components/templates/Main.tsx
@@ -6,18 +6,22 @@ function Main({ posts }: PostsResponse) {
 
   return (
     <div className=" flex flex-wrap justify-between mt-5 w-[calc(100%-200px)]">
-      {posts?.map((post) => (
-        <div key={post._id} className="w-82.5 flex justify-between border border-gray-200 m-2.5 rounded-sm ">
-          <div className="flex justify-between flex-col">
-            <p>{post.options.title}</p>
-            <div className="text-gray-400 text-sm">
-              <p>{sp(post.amount)} تومان</p>
-              <span>{post.options?.city}</span>
+      {posts?.map((post) => {
+        const title = post.title ?? post.options?.title ?? "بدون عنوان";
+        const city = post.city ?? post.options?.city ?? "";
+        return (
+          <div key={post._id} className="w-82.5 flex justify-between border border-gray-200 m-2.5 rounded-sm ">
+            <div className="flex justify-between flex-col">
+              <p>{title}</p>
+              <div className="text-gray-400 text-sm">
+                <p>{sp(post.amount)} تومان</p>
+                <span>{city}</span>
+              </div>
             </div>
+            <img src={`${BASE_URL}${post.images[0]}`} alt="" className="w-37.5 h-32.5 rounded-xs" />
           </div>
-          <img src={`${BASE_URL}${post.images[0]}`} alt="" className="w-37.5 h-32.5 rounded-xs" />
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
