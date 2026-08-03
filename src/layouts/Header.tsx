5d46234ded1c0d47610f4fe724cc2e88526c05d1
mahyarjalalvand <mahyarjalalvand99@gmail.com>
2026-08-03 10:59:50 +0330

    add: icons

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -5,22 +5,20 @@ function Header() {
     <header className="  border-2 border-gray-100 ">
       <div className="container flex justify-between items-center py-2.5 [&_div]:flex [&_span]:flex [&_span]:items-center [&_span]:text-gray-600 [&_span]:h-12.5 [&_span>p]:ms-1.5 [&_span>p]:text-sm">
         <div>
-          {/* // todo get all project icons and use */}
           <Link to="/">
-            {/* <img src="divar.svg" alt="" className="w-11.25 me-10" /> */}
-            <span>دیوار</span>
+            <img src="/divar.svg" alt="" className="w-11.25 me-10" />
           </Link>
-          <span>
-            {/* <img src="location.svg" alt="آیکن لوکیشن" /> */}
+          <div className="center gap-2">
+            <img src="/location.svg" alt=" لوکیشن" className="size-6" />
             <p>تهران</p>
-          </span>
+          </div>
         </div>
-        <div>
+        <div className="center gap-2">
           <Link to="/auth">
-            <span>
-              <img src="profile.svg" alt="" />
+            <div className="center gap-2">
+              <img src="/profile.svg" alt="پروفایل" className="size-5" />
               <p>دیوار من</p>
-            </span>
+            </div>
           </Link>
           <Link to="/dashboard" className="bg-red-800 text-white w-20 h-10 leading-10 text-center rounded-sm ms-10">
             ثبت آگهی
